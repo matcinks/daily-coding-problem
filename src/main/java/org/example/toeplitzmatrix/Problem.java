25ba8547a48790b177eb6b23b5221f81720b193a
Mateusz Stepien <mateusz.stepien@stratega-it.de>
2024-08-10 16:45:33 +0200

    Add second solution to exercise 2

--- src/main/java/org/example/toeplitzmatrix/Problem.java
+++ src/main/java/org/example/toeplitzmatrix/Problem.java
@@ -3,18 +3,13 @@ package org.example.toeplitzmatrix;
 public class Problem {
 
     public static boolean isToeplitz(int[][] matrix) {
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                int nextI = i + 1;
-                int nextJ = j + 1;
-                int value = matrix[i][j];
-                while(nextI < matrix.length - 1 && nextJ < matrix[i].length - 1) {
-                    if (value != matrix[nextI][nextJ]) {
-                        return false;
-                    }
-                    nextJ++;
-                    nextI++;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return true;
+        }
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[i].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                    return false;
                 }
             }
         }
